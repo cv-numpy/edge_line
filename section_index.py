86c575a0ced98a660043134c9a829ff57169dcfc
HighDimensional Cost <46214170+cv-numpy@users.noreply.github.com>
2023-11-20 19:37:29 +0800

    Update section_index.py

--- section_index.py
+++ section_index.py
@@ -2,7 +2,7 @@ def relative_space(x1, x2, number):
     def stride_decimal(x1, x2, number):
         return abs(x1-x2)/number
     stride_float = stride_decimal(x1, x2, number)
-    indexes = []
+    nums = []
     for i in range(number):
-        indexes.append( int(x1+stride_float*i) )
-    return indexes
+        nums.append( int(x1+stride_float*i) )
+    return nums
